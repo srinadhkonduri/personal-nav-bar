6f06619452a7347c19caf27d6dd5d15525e6e988
srinadhkonduri <srinadhkonduri@gmail.com>
2024-09-30 19:50:50 +0530

    created a responsive personal nav bar

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('toggle-button').addEventListener('click', function() {
+    const navLinks = document.getElementById('nav-links');
+    navLinks.classList.toggle('active');
+});
